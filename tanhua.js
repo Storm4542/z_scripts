e06683e76882cef3720b07ea34b43537c5f3f35b
Ember Z <417002107@qq.com>
2022-09-27 14:18:38 +0800

    first commit.

---
+++ tanhua.js
@@ -0,0 +1,12 @@
+#!name= 探花
+
+
+[Script]
+http-response ^https:\/\/jiliapi\.xndplqq\.cn\/api\/member\/info script-path=https://raw.githubusercontent.com/89996462/Quantumult-X/main/ghs/thsp.js, requires-body=true, tag=tanhua
+
+
+[URL Rewrite]
+^http:\/\/115\.com\/\?ct=sign$ http://115.com/lx?taskdg=1 header
+
+[MITM]
+hostname = jiliapi.xndplqq.cn
